e63d8974c4366d65c12e1ed09f40401027ef7292
MattHays2 <mhays@distorted.dev>
2024-03-31 17:57:39 -0600

    Initial commit

---
+++ src/test/java/csi5324/jenkinstutorial/JenkinsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package csi5324.jenkinstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
